faf2f66a0938eb2cda13474d43f0e6fccbb9f388
yuriikrevnyi <yuriikrevnyi@gmail.com>
2016-01-12 15:42:01 +0200

    Can list files in directory

--- Filesystem.js
+++ Filesystem.js
@@ -8,4 +8,26 @@ Filesystem.prototype.get = function(path) {
   return this.fs.readFileSync(path, 'utf8');
 };
 
+/**
+ * Get list of files recursively.
+ *
+ * @param  {String} dir    Path to source directory
+ * @return {Array}         List of files
+ */
+Filesystem.prototype.getFiles = function(dir, files_) {
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files) {
+    var name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      this.getFiles(name, files_);
+    } else if (name.match(/\.js(?:on)?$/i)) {
+      // for OS compatibility. On windows all '/' and '\' will convert to '\'.
+      name = path.normalize(name);
+      files_.push(name);
+    }
+  }
+  return files_;
+};
+
 module.exports = Filesystem;
